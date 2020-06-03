06a0408be2ba71415bfe5e333e92b4aee151124d
Maksim Karatkevich <m.karatkevich@akveo.com>
2020-06-03 16:09:52 +0300

    feat(timepicker): add timepicker component

---
+++ src/framework/theme/components/calendar-kit/services/calendar-time-model.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import { range } from '../helpers';
+import { NbDateService } from './date.service';
+
+
+@Injectable()
+export class NbCalendarTimeModelService {
+  readonly HOURS_IN_DAY: number = 24;
+  readonly HOURS_IN_DAY_ALT: number = 12;
+  readonly MINUTES_AND_SECONDS: number = 60;
+  readonly AMPM = ['AM', 'PM'];
+
+  constructor(protected dateService: NbDateService<any>,
+  ) {}
+
+  getHoursInDay(isTwelveHoursFormat: boolean): string[] {
+    return isTwelveHoursFormat ?
+      range(this.HOURS_IN_DAY_ALT, i => this.formatToString(i))
+      : range(this.HOURS_IN_DAY, i => this.formatToString(i));
+  }
+
+  getFullHours(use12HoursFormat, step): Array<string> {
+    const hours: number = use12HoursFormat ? this.HOURS_IN_DAY_ALT : this.HOURS_IN_DAY;
+    const res = [];
+    const temp = [];
+
+    range(hours, value => {
+      for (let i = 0; i < 60; i += step) {
+        if (use12HoursFormat) {
+          if (value !== 0) {
+            temp.push(`${this.formatToString(value)}:${this.formatToString(i)} PM`);
+          }
+          if (value !== 12) {
+            res.push(`${this.formatToString(value)}:${this.formatToString(i)} AM`);
+          }
+        } else {
+          res.push(`${this.formatToString(value)}:${this.formatToString(i)}`);
+        }
+      }
+    });
+    return use12HoursFormat ? [...res, ...temp] : res;
+  }
+
+  getMinutesAndSeconds(): string[] {
+    return range(this.MINUTES_AND_SECONDS, i => this.formatToString(i));
+  }
+
+  getAmPm(date: Date, format: string): string {
+    const ampm: RegExpMatchArray = this.getFormattedTime(date, format).match(/AM|PM/);
+    if (ampm) {
+      return ampm[0]
+    } else {
+      return '';
+    }
+  }
+
+  getFormattedTime(date: Date, format: string): string {
+    return this.dateService.format(date, format);
+  }
+
+  formatToString(n: number): string {
+    return n < 10 ? `0${n.toString()}` : n.toString();
+  }
+}
